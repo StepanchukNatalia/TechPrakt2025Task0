77070b6734b69720f8262183cce6fdb8d689bc54
Степанчук Наталія <vt232_sno@student.ztu.edu.ua>
2025-02-06 01:27:35 +0200

    Створено проект

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введiть друге число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+
+            Console.WriteLine($"Сума чисел: {sum}");
+        }
+    }
+}
